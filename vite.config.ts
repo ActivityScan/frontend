e425aafa5fbd3ac1e7c6c8d196e3f3e4585e30a9
Maria Shalaginova <112416305+MariaShalaginova@users.noreply.github.com>
2024-10-11 04:25:30 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['react-modal'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8098',
+        changeOrigin: true,
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader('Origin', 'http://localhost:8098');
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+            proxyRes.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS';
+            proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+          });
+        // secure: false,
+        // rewrite: (path) => path.replace(/^\/clubs/, ''),
+        },
+      }
+    },
+  },
+  
+  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      // "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
+
+// function resolveAlias(arg0: { '@': string; }): import("vite").PluginOption {
+//   throw new Error('Function not implemented.');
+// }
