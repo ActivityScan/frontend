e0c005608c3aed3ad8dd0a4a682974f9fbf0f283
Maria Shalaginova <112416305+MariaShalaginova@users.noreply.github.com>
2024-11-15 08:15:02 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,32 +15,34 @@ export default defineConfig({
         changeOrigin: true,
         configure: (proxy, options) => {
           proxy.on('proxyReq', (proxyReq, req, res) => {
-            proxyReq.setHeader('Origin', 'http://31.128.41.30:8098');
-          });
+            proxyReq.setHeader('Origin', 'http://31.128.41.30:8098')
+          })
           proxy.on('proxyRes', (proxyRes, req, res) => {
-            proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-            proxyRes.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS';
-            proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
-          });
-        // secure: false,
-        // rewrite: (path) => path.replace(/^\/clubs/, ''),
+            proxyRes.headers['Access-Control-Allow-Origin'] = '*'
+            proxyRes.headers['Access-Control-Allow-Methods'] =
+              'GET,PUT,POST,DELETE,OPTIONS'
+            proxyRes.headers['Access-Control-Allow-Headers'] =
+              'Content-Type, Authorization'
+          })
+          // secure: false,
+          // rewrite: (path) => path.replace(/^\/clubs/, ''),
         },
-      }
+      },
     },
   },
-  
+
   build: {
     sourcemap: true, // Включение карт кода в режиме сборки
   },
-  
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/styles'),
       // "@components": path.resolve(__dirname, "./src/components"),
     },
   },
-});
+})
 
 // function resolveAlias(arg0: { '@': string; }): import("vite").PluginOption {
 //   throw new Error('Function not implemented.');
